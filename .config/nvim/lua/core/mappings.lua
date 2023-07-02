477e662fa9cd155d581c67ba1d43071b8780683d
Gurtej Kanwar <gurtej@mit.edu>
2023-07-02 18:54:41 +0200

    nvim: line insert keymaps

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -7,6 +7,12 @@ vim.api.nvim_set_keymap('v', '<S-CR>', '<Esc>', { noremap = true })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+-- Inserting lines
+vim.keymap.set('n', 'o', 'o<Esc>0"_D')
+vim.keymap.set('n', 'O', 'O<Esc>0"_D')
+vim.keymap.set('n', 'zj', 'o<Esc>k')
+vim.keymap.set('n', 'zk', 'O<Esc>j')
+
 -- Navigation
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
